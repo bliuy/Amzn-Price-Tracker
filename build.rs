df781579540e53e5ada0a353ffa5c1d94e89726d
Brandon Liu <liuy01510.work@gmail.com>
2023-05-14 08:41:54 +0000

    Added build script.

---
+++ build.rs
@@ -0,0 +1,10 @@
+use std::{error::Error, path::Path};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let protos: Vec<&str> = vec![r#"src/proto/messages.proto"#];
+
+    let includes: Vec<&str> = vec![r#"src/"#];
+
+    prost_build::compile_protos(&protos, &includes)?;
+    Ok(())
+}
