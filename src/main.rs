010e7df1aa11eacadb055d96cfb17ce3a1683cdc
Brandon Liu <liuy01510.work@gmail.com>
2023-05-28 08:14:25 +0000

    Modifying services to coordinate with Pubsub.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use crate::structs::AmznProductInformation;
 use actix_web::{guard, web, App, HttpServer};
 use proto_messages::AmznScrapingRequest;
 use reqwest::ClientBuilder;
-use rumqttc::{AsyncClient, Client, Event, MqttOptions, Packet};
 
 mod amzn;
 
@@ -20,15 +19,18 @@ async fn main() {
                 ClientBuilder::new().user_agent(USER_AGENT).build().unwrap(),
             ))
             .service(
-                web::resource("/hello_world")
-                    .route(web::route().guard(guard::Get()).to(services::hello_world)),
+                web::resource("/hello_world").route(
+                    web::route()
+                        .guard(guard::Any(guard::Get()).or(guard::Post())) // Allowing POST - Used for PubSub.
+                        .to(services::hello_world),
+                ),
             )
             .service(
                 web::resource("/amzn_request")
                     .route(web::route().guard(guard::Post()).to(services::amzn_request)),
             )
     })
-    .bind(("localhost", 8080))
+    .bind(("0.0.0.0", 8080))
     .unwrap()
     .run()
     .await
@@ -40,9 +42,10 @@ mod services {
 
     use actix_web::{
         rt::spawn,
-        web::{self, Bytes},
+        web::{self, Bytes, Json},
         HttpRequest, HttpResponse, Responder, ResponseError, Result,
     };
+    use base64::{engine::general_purpose, Engine};
     use futures::future;
     use reqwest::Client;
     use tokio::time::sleep;
@@ -50,6 +53,7 @@ mod services {
     use crate::{
         amzn::{parse_response, scrape, successful_scrape, unsuccessful_scrape},
         proto_messages::AmznScrapingRequest,
+        pubsub::PubSubMessage,
         structs::AmznProductInformation,
     };
 
@@ -59,11 +63,34 @@ mod services {
 
     pub async fn amzn_request(
         reqwest_client: web::Data<Client>,
-        request: HttpRequest,
-        request_payload: web::Bytes,
+        request_payload: Json<PubSubMessage>,
     ) -> Result<String> {
-        let amzn_scraping_request: AmznScrapingRequest = prost::Message::decode(request_payload)
-            .map_err(|e| actix_web::error::ErrorBadRequest(e))?;
+        // Printing out the Json Payload for debugging purposes
+        println!("{:#?}", request_payload);
+
+        let encoded_payload = request_payload.message.data.as_bytes();
+        let amzn_scraping_request: AmznScrapingRequest = match general_purpose::STANDARD
+            .decode(encoded_payload)
+        {
+            Ok(i) => {
+                match prost::Message::decode(i.as_ref()) {
+                    Ok(i) => i,
+                    Err(e) => {
+                        println!(
+                            "Unable to decode message into intended class. See error: {}",
+                            e
+                        );
+                        return Ok("Message was received but was unable to be decoded.".to_string());
+                        // Incorrect schema detected; no point in retrying.
+                    }
+                }
+            }
+            Err(e) => {
+                println!("Base64 decoding failed. See error: {}", e);
+                return Ok("Message was received but was unable to be decoded.".to_string());
+                // Incorrect schema detected; no point in retrying.
+            }
+        };
 
         let futures = amzn_scraping_request
             .product_codes
@@ -99,11 +126,12 @@ mod services {
             Ok(i) => {
                 let successful: u32 = i.iter().sum();
                 let total = i.len();
-                return Ok(format!("Status: {}/{} requests were successfully scraped.", successful, total))
-            },
-            Err(e) => {
-                return Err(actix_web::error::ErrorBadRequest(e))
-            },
+                return Ok(format!(
+                    "Status: {}/{} requests were successfully scraped.",
+                    successful, total
+                ));
+            }
+            Err(e) => return Err(actix_web::error::ErrorBadRequest(e)),
         };
     }
 }
@@ -117,3 +145,24 @@ async fn amzn_scraper(product: AmznProductInformation) -> () {
 }
 
 mod structs;
+
+mod pubsub {
+    use std::collections::HashMap;
+
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct PubSubMessage {
+        pub(crate) message: PubSubMessageMessage,
+        pub(crate) subscription: String,
+    }
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct PubSubMessageMessage {
+        pub(crate) attributes: HashMap<String, String>,
+        pub(crate) data: String,         // Base64 encoded
+        pub(crate) messageId: String,    // Appears to be identical to message_id
+        pub(crate) message_id: String,   // String of numbers
+        pub(crate) publishTime: String,  // Appears to be identical to publish_time
+        pub(crate) publish_time: String, // Timestamp when the message was published. In the following format: YYYY-MM-DDTHH:MM:SS.sssZ (Example: 2021-02-26T19:13:55.749Z)
+    }
+}
